b43e34a2ab2c48cb9a7d7f5d79993e21141b56f8
Riyan-Gupta <113873665+Riyan-Gupta@users.noreply.github.com>
2023-08-27 08:55:22 +0530

    Update DieRoll.cpp

--- DieRoll.cpp
+++ DieRoll.cpp
@@ -1,1 +1,23 @@
 #include <iostream>
+using namespace std;
+
+int main() {
+   int a;
+   cin>>a;
+   int b;
+   cin>>b;
+   int c;
+   if(a>=b){
+     c=a;
+   }
+   else{
+     c=b;
+   }
+   int j=0;
+   for(int i=c;i<=6;i++){
+       j++;
+   }
+   cout<<j/6;
+   
+   
+     
