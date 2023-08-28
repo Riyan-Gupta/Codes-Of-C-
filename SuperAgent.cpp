ba560540db91018ed8fce1acb5bff20718dc9682
Riyan-Gupta <113873665+Riyan-Gupta@users.noreply.github.com>
2023-08-28 14:43:08 +0530

    Update SuperAgent.cpp

--- SuperAgent.cpp
+++ SuperAgent.cpp
@@ -1,2 +1,25 @@
-#include <iostream>;
+#include <iostream>
+using namespace std;
 
+int main() {
+   char a[3][3];
+   for(int i=0;i<=2;i++){
+       for(int j=0;j<=2;j++){
+           if(a[i][j]=='X' || a[i][j]=='.')
+           {
+           cin>>a[i][j];
+           }
+       }
+   }
+   for(int i=0;i<=1;i++){
+      for(int j=1;j<=2;j++){
+        if(a[0][0]==a[2][2]=='X' && a[i][j]==a[3-i][3-j]=='X' || '.'){
+           cout<<"YES";
+        }
+        else{
+           cout<<"NO";
+        }
+      }
+   }
+
+   
